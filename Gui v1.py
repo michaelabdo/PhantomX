2904409d21fd4b96b727e0be45bd496e7dbbf4f8
JoshuaCraig <36288290+JoshuaCraig@users.noreply.github.com>
2019-08-27 11:07:10 +1000

    Add files via upload
    changes to how to access tables, also added the spacing for the tables

--- Gui v1.py
+++ Gui v1.py
@@ -8,7 +8,10 @@
 
 from tkinter import *
 from tkinter import ttk
-columns = ["column1", "column2", "column3", "column4"]
+import re
+
+columns = ["column 1", "column 2", "column 3", "column 4"]
+
 class alphaFrame:
     
     def __init__(self, Root):
@@ -21,26 +24,34 @@ class alphaFrame:
         self.topFrame.config(relief = SOLID)
         
         #Text input to take the input from user, each column relates to a physical column for the arm
-        
+        column1 = Text(self.topFrame, width = 10, height = 1)
+        column2 = Text(self.topFrame, width = 10, height = 1)
+        column3 = Text(self.topFrame, width = 10, height = 1)
+        column4 = Text(self.topFrame, width = 10, height = 1)
+        column1.grid(row = 1, column = 0, columnspan = 1, padx = 10, pady = 10)
+        column2.grid(row = 1, column = 1, columnspan = 1, padx = 10, pady = 10)
+        column3.grid(row = 1, column = 2, columnspan = 1, padx = 10, pady = 10)
+        column4.grid(row = 1, column = 3, columnspan = 1, padx = 10, pady = 10)
         y = 0
+
+        #loop to place all the labels for the columns above the input
         for x in columns:
-            x = Text(self.topFrame, width = 10, height = 1)
-            x.grid(row = 0, column = y, columnspan = 1)
+            label = ttk.Label(self.topFrame, text = x)
+            label.grid(row = 0, column = y, columnspan = 1)
             y = y + 1
-            
         
+        
+        
+            
         #Padding prevents frame from collapsing around button, err known on mac
         self.topFrame.config(padding = (30,50))
         
-        #Frame defined as buffer for future dev, for visuals or other features, ignore or delete
+        #Frame defined as buffer for future dev, for visuals or other features (err messages), ignore or delete
         
         #self.middleFrame = ttk.Frame(Root)
         #self.middleFrame.pack(fill = BOTH)
         #self.middleFrame.config(relief = SOLID)
         
-        #reset = ttk.Button(self.middleFrame, text = 'reset')
-        #reset.grid(row = 0, column = 0, columnspan = 1)
-        #self.middleFrame.config(padding = (30,50))
 
         #
         
