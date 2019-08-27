170c7fef3c550517bf3b4008fd8c7e251cf2dc4f
michaelabdo <47989996+michaelabdo@users.noreply.github.com>
2019-08-27 13:59:26 +1000

    Add files via upload
    Add SpinBoxs

--- Gui v1.py
+++ Gui v1.py
@@ -10,23 +10,14 @@ from tkinter import *
 from tkinter import ttk
 import re
 
-columns = ["column 1", "column 2", "column 3", "column 4"]
+platforms = ["Platform 1", "Platform 2", "Platform 3", "Platform 4"]
 
-#reset grid and values function
-def resetGrid():
-    print('reset')
-
-#run arm(send values to text file for asp)
-def runArm():
-    print('run arm')
-    
 class alphaFrame:
-    
-        
+
     def __init__(self, Root):
         #sets size of window on open
-        Root.geometry('500x500')
-        
+        Root.geometry('900x500')
+
         #Frame is set to use Pack method, to fill space across with a border
         self.topFrame = ttk.Frame(Root)
         self.topFrame.pack(fill = BOTH)
@@ -38,43 +29,45 @@ class alphaFrame:
 
         for z in range(6):
             for x in range(4):
-                x = Text(self.topFrame, width = 10, height = 1)
-                x.grid(row = z+1, column = y, columnspan = 1, padx = 10, pady = 10)
+                #x = Text(self.topFrame, width = 10, height = 1)
+                #x.grid(row = z+1, column = y, columnspan = 1, padx = 10, pady = 10)
+                w = Spinbox(self.topFrame, from_=0, to=6)
+                w.grid(row = z+1, column = y, columnspan = 1, padx = 10, pady = 10)
                 y = y + 1
             y = 0
         y = 0
 
         #loop to place all the labels for the columns above the input
-        for x in columns:
+        for x in platforms:
             label = ttk.Label(self.topFrame, text = x)
             label.grid(row = 0, column = y, columnspan = 1)
             y = y + 1
-        
-        
-        
-            
+
+
+
+
         #Padding prevents frame from collapsing around button, err known on mac
         self.topFrame.config(padding = (30,50))
-        
+
         #Frame defined as buffer for future dev, for visuals or other features (err messages), ignore or delete
-        
+
         #self.middleFrame = ttk.Frame(Root)
         #self.middleFrame.pack(fill = BOTH)
         #self.middleFrame.config(relief = SOLID)
-        
+
 
         #
-        
+
         #Frame is directly under topframe by order of pack methods called
         self.botomFrame = ttk.Frame(Root)
         self.botomFrame.pack(fill = BOTH)
         self.botomFrame.config(relief = SOLID)
-        
+
         #future reset and run buttons
-        reset = ttk.Button(self.botomFrame, text = 'reset', command = resetGrid)
+        reset = ttk.Button(self.botomFrame, text = 'reset')
         reset.grid(row = 0, column = 0, columnspan = 1)
-        
-        run = ttk.Button(self.botomFrame, text = 'run', command = runArm)
+
+        run = ttk.Button(self.botomFrame, text = 'run')
         run.grid(row = 0, column = 2, columnspan = 1)
         self.botomFrame.config(padding = (30,50))
 
@@ -82,5 +75,9 @@ class alphaFrame:
 def main():
     root = Tk()
     app = alphaFrame(root)
+    root.title("PhantomX GUI")
+
+
     root.mainloop()
+
 if __name__ == "__main__": main()
